eade033eb6f8b39816b2bdf17b35e00e4aec3a44
Frédéric Le Coquil <flecoqui@microsoft.com>
2018-01-10 10:18:38 +0100

    Add TestRTMP Project to stream RTMP content towards Azure without authentication

---
+++ RTMPPublisher/Samples/TestRTMP/CaptureDevice.cs
@@ -0,0 +1,306 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Microsoft.Media.RTMP;
+
+namespace TestRTMP
+{
+    public class MediaSink : IDisposable
+    {
+       // Windows.Media.IMediaExtension mediaExtension;
+        //public Windows.Media.IMediaExtension GetMFExtensions()
+        //{
+        //    return mediaExtension;
+
+        //}
+        PublishProfile publishProfile;
+        RTMPPublishSession session;
+        public   Windows.Foundation.IAsyncOperation<Windows.Media.IMediaExtension> InitializeAsync(string uri, AudioEncodingProperties audioProf, VideoEncodingProperties videoProf )
+
+        {
+            Windows.Foundation.IAsyncOperation<Windows.Media.IMediaExtension> result = null;
+
+            //inputprof.Audio.Bitrate = 96000;
+            //inputprof.Audio.BitsPerSample = 16;
+            //inputprof.Audio.ChannelCount = 2;
+            //inputprof.Audio.SampleRate = 44100;
+            //inputprof.Audio.Subtype = "AAC";
+            //inputprof.Video.Bitrate = 400000;
+            //inputprof.Video.Height = 240;
+            //inputprof.Video.Width = 320;
+            //inputprof.Video.Subtype = "H264";
+            //inputprof.Video.ProfileId = 77;
+
+            publishProfile = new PublishProfile(RTMPServerType.Azure);
+            publishProfile.TargetEncodingProfile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.HD720p);
+            publishProfile.TargetEncodingProfile.Container = null;
+
+
+                publishProfile.EndpointUri = uri;
+                publishProfile.TargetEncodingProfile.Video.Width = 320;
+                publishProfile.TargetEncodingProfile.Video.Height = 240;
+                publishProfile.TargetEncodingProfile.Video.Bitrate = 400000;
+
+ //               publishProfile.KeyFrameInterval = kfinterval.Value;
+//                publishProfile.ClientChunkSize = chunksize.Value;
+
+            //                publishProfile.TargetEncodingProfile.Video.ProfileId = profile.ToLowerInvariant() == "baseline" ? H264ProfileIds.Baseline : (profile.ToLowerInvariant() == "main" ? H264ProfileIds.Main : H264ProfileIds.High);
+
+            publishProfile.TargetEncodingProfile.Video.ProfileId = H264ProfileIds.Baseline ;
+            publishProfile.TargetEncodingProfile.Video.FrameRate.Numerator = videoProf.FrameRate.Numerator;
+            publishProfile.TargetEncodingProfile.Video.FrameRate.Denominator = videoProf.FrameRate.Denominator;
+
+                publishProfile.TargetEncodingProfile.Audio.Bitrate = 96000;
+                publishProfile.TargetEncodingProfile.Audio.ChannelCount = 2U;
+                publishProfile.TargetEncodingProfile.Audio.SampleRate = 44100;
+
+
+            System.Collections.Generic.List<PublishProfile> list = new System.Collections.Generic.List<PublishProfile>();
+            list.Add(publishProfile);
+            session = new RTMPPublishSession(list);
+            if(session!=null)
+            {
+                result = session.GetCaptureSinkAsync();
+            }
+            return result;
+        }
+        public void Dispose()
+        {
+
+        }
+    }
+    internal class CaptureDevice
+    {
+        // Media capture object
+        private MediaCapture mediaCapture;
+        // Custom media sink
+        private MediaSink mediaSink;
+        // Flag indicating if recording to custom sink has started
+        private bool recordingStarted = false;
+        private bool forwardEvents = false;
+
+        // Wraps the capture failed and media sink incoming connection events        
+        public event EventHandler<MediaCaptureFailedEventArgs> CaptureFailed;
+
+        public CaptureDevice() { }
+
+        /// <summary>
+        ///  Handler for the wrapped MediaCapture object's Failed event. It just wraps and forward's MediaCapture's 
+        ///  Failed event as own CaptureFailed event
+        /// </summary>
+        private void mediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            if (CaptureFailed != null && forwardEvents) CaptureFailed(this, errorEventArgs);
+        }
+
+
+        /// <summary>
+        /// Cleans up the resources.
+        /// </summary>
+        private void CleanupSink()
+        {
+            if (mediaSink != null)
+            {
+                mediaSink.Dispose();
+                mediaSink = null;
+                recordingStarted = false;
+            }
+        }
+
+        private void DoCleanup()
+        {
+            if (mediaCapture != null)
+            {
+                mediaCapture.Failed -= mediaCapture_Failed;
+                mediaCapture = null;
+            }
+
+            CleanupSink();
+        }
+
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                forwardEvents = true;
+
+                if (mediaCapture != null)
+                {
+                    throw new InvalidOperationException("Camera is already initialized");
+                }
+
+                mediaCapture = new MediaCapture();
+                mediaCapture.Failed += mediaCapture_Failed;
+
+                await mediaCapture.InitializeAsync();
+            }
+            catch (Exception e)
+            {
+                DoCleanup();
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Asynchronous method cleaning up resources and stopping recording if necessary.
+        /// </summary>
+        public async Task CleanUpAsync()
+        {
+            try
+            {
+                forwardEvents = true;
+
+                if (mediaCapture == null && mediaSink == null) return;
+
+                if (recordingStarted)
+                {
+                    await mediaCapture.StopRecordAsync();
+                }
+
+                DoCleanup();
+            }
+            catch (Exception)
+            {
+                DoCleanup();
+            }
+        }
+
+        /// <summary>
+        /// Creates url object from MediaCapture
+        /// </summary>
+        public MediaCapture CaptureSource
+        {
+            get { return mediaCapture; }
+        }
+
+        /// <summary>
+        /// Allow selection of camera settings.
+        /// </summary>
+        /// <param name="mediaStreamType" type="Windows.Media.Capture.MediaStreamType">
+        /// Type of a the media stream.
+        /// </param>
+        /// <param name="filterSettings" type="Func<Windows.Media.MediaProperties.IMediaEncodingProperties, bool>">
+        /// A predicate function, which will be called to filter the correct settings.
+        /// </param>
+        public async Task<IMediaEncodingProperties> SelectPreferredCameraStreamSettingAsync(MediaStreamType mediaStreamType, Func<IMediaEncodingProperties, bool> filterSettings)
+        {
+            IMediaEncodingProperties previewEncodingProperties = null;
+
+            if (mediaStreamType == MediaStreamType.Audio || mediaStreamType == MediaStreamType.Photo)
+            {
+                throw new ArgumentException("mediaStreamType value of MediaStreamType.Audio or MediaStreamType.Photo is not supported", "mediaStreamType");
+            }
+            if (filterSettings == null)
+            {
+                throw new ArgumentNullException("filterSettings");
+            }
+
+            var properties = mediaCapture.VideoDeviceController.GetAvailableMediaStreamProperties(mediaStreamType);
+            var filterredProperties = properties.Where(filterSettings);
+            var preferredSettings = filterredProperties.ToArray();
+
+            Array.Sort<IMediaEncodingProperties>(preferredSettings, (x, y) =>
+            {
+                return (int)(((x as VideoEncodingProperties).Width) -
+                    (y as VideoEncodingProperties).Width);
+            });
+
+            if (preferredSettings.Length > 0)
+            {
+                previewEncodingProperties = preferredSettings[0];
+                await mediaCapture.VideoDeviceController.SetMediaStreamPropertiesAsync(mediaStreamType, preferredSettings[0]);
+            }
+
+            return previewEncodingProperties;
+        }
+
+        /// <summary>
+        /// Starts media recording asynchronously
+        /// </summary>
+        /// <param name="encodingProfile">
+        /// Encoding profile used for the recording session
+        /// </param>
+        public async Task StartRecordingAsync(string uri, MediaEncodingProfile encodingProfile)
+        {
+            try
+            {
+                // We cannot start recording twice.
+                if (mediaSink != null && recordingStarted)
+                {
+                    throw new InvalidOperationException("Recording already started.");
+                }
+
+                // Release sink if there is one already.
+                CleanupSink();
+
+                // Create new sink
+                mediaSink = new MediaSink();
+
+
+                var mfExtension = await mediaSink.InitializeAsync(uri, encodingProfile.Audio, encodingProfile.Video);
+
+              //  Windows.Storage.StorageFile storageFile = await Windows.Storage.KnownFolders.VideosLibrary.CreateFileAsync("capture1.mp4", Windows.Storage.CreationCollisionOption.GenerateUniqueName);
+                //Windows.Storage.StorageFile storageFile = await Windows.Storage.StorageFile.GetFileFromPathAsync("C:\\Users\\flecoqui\\Videos\\capture.mp4");
+                //MediaEncodingProfile recordProfile = null;
+                //recordProfile = MediaEncodingProfile.CreateMp4(Windows.Media.MediaProperties.VideoEncodingQuality.Auto);
+
+                //await mediaCapture.StartRecordToStorageFileAsync(recordProfile, storageFile);
+                    //   await mediaCapture.StartRecordToCustomSinkAsync(recordProfile, mfExtension);
+                       await mediaCapture.StartRecordToCustomSinkAsync(encodingProfile, mfExtension);
+
+                recordingStarted = true;
+            }
+            catch (Exception e)
+            {
+                CleanupSink();
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Stops recording asynchronously
+        /// </summary>
+        public async Task StopRecordingAsync()
+        {
+            if (recordingStarted)
+            {
+                try
+                {
+                    await mediaCapture.StopRecordAsync();
+                    CleanupSink();
+                }
+                catch (Exception)
+                {
+                    CleanupSink();
+                }
+            }
+        }
+
+        public static async Task<bool> CheckForRecordingDeviceAsync()
+        {
+            var cameraFound = false;
+
+            var devices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            if (devices.Count > 0)
+            {
+                cameraFound = true;
+            }
+
+            return cameraFound;
+        }
+    }
+}
---
+++ RTMPPublisher/Samples/TestRTMP/MainPage.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Media.RTMP;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestRTMP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        CaptureDevice device = null;
+        bool bStreaming = false;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LogMessage("MainPage constructor...");
+
+        }
+        void UpdateControls()
+        {
+            if (bStreaming == true)
+            {
+                StartStreaming.IsEnabled = false;
+                RTMPUri.IsEnabled = false;
+                StopStreaming.IsEnabled = true;
+            }
+            else
+            {
+                StartStreaming.IsEnabled = true;
+                RTMPUri.IsEnabled = true;
+                StopStreaming.IsEnabled = false;
+            }
+
+        }
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Logs event to refresh the TextBox
+            logs.TextChanged += Logs_TextChanged;
+
+            UpdateControls();
+        }
+        async void Device_CaptureFailed(object sender, Windows.Media.Capture.MediaCaptureFailedEventArgs e)
+        {
+            LogMessage("Device Capture Failed");
+            if (device != null)
+            {
+                await device.StopRecordingAsync();
+                await device.CleanUpAsync();
+                bStreaming = false;
+                device = null;
+            }
+        }
+        protected async override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+
+            if (device != null)
+            {
+                await device.CleanUpAsync();
+                device = null;
+            }
+        }
+        private async void StartStreaming_Click(object sender, RoutedEventArgs e)
+        {
+            //            string rtmpUri = "rtmp://testrtmplivearena-testlivearenamd.channel.mediaservices.windows.net:1935/live/ca620842c33e4371a6071e5b12f705df/MyStream1";
+            string rtmpUri = "rtmp://testrtmplivearena-testlivearenamd.channel.mediaservices.windows.net:1935/live/ca620842c33e4371a6071e5b12f705df";
+            // string previewUri = "http://testrtmplivearena-testlivearenamd.channel.mediaservices.windows.net/preview.isml/manifest";
+            if (!string.IsNullOrEmpty(RTMPUri.Text))
+                LogMessage("Start Streaming towards " + RTMPUri.Text);
+            else
+                LogMessage("Start Streaming...");
+            var cameraFound = await CaptureDevice.CheckForRecordingDeviceAsync();
+
+            if (cameraFound)
+            {
+                if(device!=null)
+                {
+                    await device.StopRecordingAsync();
+                    await device.CleanUpAsync();
+                }
+                device = new CaptureDevice();
+                if (device != null)
+                {
+                    try
+
+                    {
+                        await device.InitializeAsync();
+                        Windows.Media.MediaProperties.MediaEncodingProfile mep = Windows.Media.MediaProperties.MediaEncodingProfile.CreateMp4(Windows.Media.MediaProperties.VideoEncodingQuality.Qvga);
+
+                        mep.Video.FrameRate.Numerator = 15;
+                        mep.Video.FrameRate.Denominator = 1;
+                        mep.Container = null;
+                        if (!string.IsNullOrEmpty(RTMPUri.Text))
+                            rtmpUri = RTMPUri.Text;
+                        await device.StartRecordingAsync(rtmpUri, mep);
+                        device.CaptureFailed += Device_CaptureFailed;
+                        bStreaming = true;
+                        LogMessage("Start Streaming successful");
+                    }
+                    catch (Exception ex)
+                    {
+                        LogMessage("Start Streaming Exception: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                LogMessage("A machine with a camera and a microphone is required to run this sample.");
+            }
+        }
+        private async void StopStreaming_Click(object sender, RoutedEventArgs e)
+        {
+            LogMessage("Stop Streaming...");
+            try
+            {
+                if (device != null)
+                {
+                    await device.StopRecordingAsync();
+                    await device.CleanUpAsync();
+                    bStreaming = false;
+                    device = null;
+                    LogMessage("Stop Streaming successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogMessage("Stop Streaming Exception: " + ex.Message);
+            }
+        }
+
+
+        #region Logs
+        void PushMessage(string Message)
+        {
+            App app = Windows.UI.Xaml.Application.Current as App;
+            if (app != null)
+                app.MessageList.Enqueue(Message);
+        }
+        bool PopMessage(out string Message)
+        {
+            Message = string.Empty;
+            App app = Windows.UI.Xaml.Application.Current as App;
+            if (app != null)
+                return app.MessageList.TryDequeue(out Message);
+            return false;
+        }
+        /// <summary>
+        /// Display Message on the application page
+        /// </summary>
+        /// <param name="Message">String to display</param>
+        async void LogMessage(string Message)
+        {
+            string Text = string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + " " + Message + "\n";
+            PushMessage(Text);
+            System.Diagnostics.Debug.WriteLine(Text);
+            await DisplayLogMessage();
+        }
+        /// <summary>
+        /// Display Message on the application page
+        /// </summary>
+        /// <param name="Message">String to display</param>
+        async System.Threading.Tasks.Task<bool> DisplayLogMessage()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                () =>
+                {
+
+                    string result;
+                    while (PopMessage(out result))
+                    {
+                        logs.Text += result;
+                        if (logs.Text.Length > 16000)
+                        {
+                            string LocalString = logs.Text;
+                            while (LocalString.Length > 12000)
+                            {
+                                int pos = LocalString.IndexOf('\n');
+                                if (pos == -1)
+                                    pos = LocalString.IndexOf('\r');
+
+
+                                if ((pos >= 0) && (pos < LocalString.Length))
+                                {
+                                    LocalString = LocalString.Substring(pos + 1);
+                                }
+                                else
+                                    break;
+                            }
+                            logs.Text = LocalString;
+                        }
+                    }
+                }
+            );
+            return true;
+        }
+        /// <summary>
+        /// This method is called when the content of the Logs TextBox changed  
+        /// The method scroll to the bottom of the TextBox
+        /// </summary>
+        void Logs_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //  logs.Focus(FocusState.Programmatic);
+            // logs.Select(logs.Text.Length, 0);
+            var tbsv = GetFirstDescendantScrollViewer(logs);
+            tbsv.ChangeView(null, tbsv.ScrollableHeight, null, true);
+        }
+        /// <summary>
+        /// Retrieve the ScrollViewer associated with a control  
+        /// </summary>
+        ScrollViewer GetFirstDescendantScrollViewer(DependencyObject parent)
+        {
+            var c = VisualTreeHelper.GetChildrenCount(parent);
+
+            for (int i = 0; i < c; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                var sv = child as ScrollViewer;
+                if (sv != null)
+                    return sv;
+                sv = GetFirstDescendantScrollViewer(child);
+                if (sv != null)
+                    return sv;
+            }
+
+            return null;
+        }
+        #endregion
+
+    }
+}
